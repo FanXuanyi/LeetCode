5a1b5f88816009a7a0a14439d515af7bedbf76c3
FanXuanyi <lollipop94ffxy@163.com>
2017-12-05 15:15:06 +0800

    237. Delete Node in a Linked List

---
+++ LinkedList/237. Delete Node in a Linked List.js
@@ -0,0 +1,32 @@
+/*
+ Write a function to delete a node (except the tail) in a singly linked list,
+ given only access to that node.
+
+Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+ */
+
+/*
+题目：
+写一个方法，移除单向链表的一个节点（最后一个节点除外）。
+
+分析：
+既然要移除当前节点，就是将前一个节点直接指向下一个节点，
+所以，将节点的值赋为下一个节点的值，
+将节点的指向next赋为下一个节点的指向，即node.next.next。
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
+};
