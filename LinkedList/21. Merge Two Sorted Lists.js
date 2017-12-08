f67aa0c23a0fbcbfb3f9cfc09bebc893773dde64
FanXuanyi <lollipop94ffxy@163.com>
2017-12-08 20:05:43 +0800

    21.Merge Two Sorted Lists

---
+++ LinkedList/21. Merge Two Sorted Lists.js
@@ -0,0 +1,59 @@
+/*
+Merge two sorted linked lists and return it as a new list.
+The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+ */
+
+/*
+题目：
+合并两个有序链表，返回合并后的新链表。
+
+分析：
+首先，判断两个链表中是否存在空链表，
+如果有一个链表为空，则不需要合并过程，返回另一个链表的头节点即可。
+如果两个链表都不为空，则继续如下操作：
+比较l1和l2的值，较小值的节点为合并后链表的头节点，记为head，
+然后，将head为头节点的链表记为cur1，另一链表即为cur2，
+将两个链表从头到尾开始比较，
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    if (l1 === null || l2 === null) {
+        return l1 !== null ? l1 : l2;
+    }
+    var head = l1.val < l2.val ? l1 : l2;
+    var cur1 = head === l1 ? l1 : l2;
+    var cur2 = head === l1 ? l2 : l1;
+    var pre = null,
+        next = null;
+    while (cur1 && cur2) {
+        if (cur1.val <= cur2.val) {
+            pre = cur1;
+            cur1 = cur1.next;
+        }
+        else {
+            next = cur2.next;
+            pre.next = cur2;
+            cur2.next = cur1;
+            pre = cur2;
+            cur2 = next;
+        }
+    }
+    pre.next = cur1 === null ? cur2 : cur1;
+    return head;
+};
