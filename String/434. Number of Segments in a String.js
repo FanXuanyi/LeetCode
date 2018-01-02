112aaae26f4fe5c32db920bbcafd21fbbfe55a86
FanXuanyi <lollipop94ffxy@163.com>
2018-01-02 14:01:36 +0800

    434. Number of Segments in a String

--- String/434. Number of Segments in a String.js
+++ String/434. Number of Segments in a String.js
@@ -9,6 +9,21 @@ Input: "Hello, my name is John"
 Output: 5
  */
 
+/*
+题目：
+统计一个字符串的分段数量，一个连续没有空格的字符集为一个分段。
+
+分析：
+看到这一题很容易想到557. Reverse Words in a String III这一题，
+不同的是，不需要反转单词，只需统计单词的数量，
+这里特别需要注意的是连续空格的情况，
+具体的做法是：
+遍历字符串，遇到空格直接跳过，如果不是空格，则count加1，
+然后用一个while循环找到下一个空格的位置，
+这样就遍历完了一个单词，
+再重复上面的操作直至结束。
+ */
+
 /**
  * @param {string} s
  * @return {number}
@@ -25,6 +40,22 @@ var countSegments = function(s) {
         count++;
         while (i < s.length && s[i] !== ' ') {
             i++;
+        }
+    }
+    return count;
+};
+
+/*
+换一种方法，统计每个单词的第一个字符，
+因为每一个单词的第一个字符不是空格，并且它前面的一个字符一个是空格，
+或是它是第一个单词，
+利用这一特点可以统计出单词的个数。
+ */
+var countSegments = function(s) {
+    var count = 0;
+    for (let i=0; i<s.length; i++) {
+        if (s[i] !== " " && (i===0 || s[i-1]===' ')) {
+            count++;
         }
     }
     return count;
